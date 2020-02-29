0853a5a7b488283016036a1cdd42ff6b34476c5a
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-02-29 23:18:29 +0100

    navbar újratervezése

--- your-time/src/components/Nav.js
+++ your-time/src/components/Nav.js
@@ -1,28 +1,40 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import Container from "@material-ui/core/Container";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import Link from "@material-ui/core/Link";
-import ButtonBase from "@material-ui/core/ButtonBase"
+import NavListItem from "./NavListItem";
+import TableChartIcon from '@material-ui/icons/TableChart';
+import NoteIcon from '@material-ui/icons/Note';
+import CodeIcon from '@material-ui/icons/Code';
+import PortraitIcon from '@material-ui/icons/Portrait';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
+    display: "flex"
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3)
   },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  },
-  link: {
-    flexGrow: 1
-  }
+  toolbar: theme.mixins.toolbar
 }));
 
 const Nav = () => {
@@ -30,56 +42,30 @@ const Nav = () => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Container>
-          <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              YourTime
-            </Typography>
-            <ButtonBase  variant="h6" className={classes.link}>
-            <Typography>
-              Órarend tervező
-            </Typography>
-            </ButtonBase>
-            <ButtonBase  variant="h6" className={classes.link}>
-            <Typography>
-              Cetlik
-            </Typography>
-            </ButtonBase>
-            <ButtonBase  variant="h6" className={classes.link}>
-            <Typography>
-              Magamról
-            </Typography>
-            </ButtonBase>
-            <div>
-              <Link
-                href="https://github.com/keserukristof/SZAKDOLGOZAT"
-                color="inherit"
-              >
-                <IconButton
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <GitHubIcon />
-                </IconButton>
-              </Link>
-              <Link
-                href="https://www.facebook.com/keserukristof"
-                color="inherit"
-              >
-                <IconButton
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  color="inherit"
-                >
-                  <FacebookIcon />
-                </IconButton>
-              </Link>
-            </div>
-          </Toolbar>
-        </Container>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          <NavListItem text="Órarendtervező" icon={<TableChartIcon />} />
+          <NavListItem text="Cetlik" icon={<NoteIcon />} />
+          <NavListItem text="A programról" icon={<CodeIcon />} />
+          <NavListItem text="A készítőről" icon={<PortraitIcon />} />
+          <NavListItem text="Github" icon={<GitHubIcon />} />
+        </List>
+      </Drawer>
     </div>
   );
 };
