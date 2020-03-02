a77db36b58ab269aa85f8c6fe6f54f945bfd81f0
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-03-02 16:56:10 +0100

    navigáció átrendezés elkezdése

--- your-time/src/App.js
+++ your-time/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Nav from "./components/Nav"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Nav from "./components/Nav";
+import AboutTheProgram from "./pages/AboutTheProgram";
 
 const App = () => {
   return (
@@ -8,10 +14,12 @@ const App = () => {
       <div className="App">
         <Nav />
         <Switch>
-          <Route path="/" exact />
-          <Route path="/createTimeTable" />
-          <Route path="/notes" />
-          <Route path="/about" />
+            <Route path="/" exact />
+            <Route path="/time-table" />
+            <Route path="/notes" />
+            <Route path="/about-the-program" />
+            <Route path="/about-the-author" />
+            <Redirect to="/" />
         </Switch>
       </div>
     </Router>
---
+++ your-time/src/pages/AboutTheProgram.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutTheProgram = () => {
+  return (
+    <div>
+      <h1>PROGRAM</h1>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+      <h2>PROGRAM</h2>
+    </div>
+  );
+};
+
+export default AboutTheProgram;
