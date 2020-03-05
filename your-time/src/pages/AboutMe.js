4aa54dbdfc882ef38979d18fde84bb30ee06786a
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-03-05 10:59:10 +0100

    about me page elkészítése

--- your-time/src/pages/AboutMe.js
+++ your-time/src/pages/AboutMe.js
@@ -1,10 +1,35 @@
 import React from "react";
+import CostumCard from "../components/CostumCard";
+import { Grid } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>ABOUT ME</h1>
-    </div>
+    <Grid container spacing={5}>
+      <Grid item xs>
+        <CostumCard
+          image={require("../images/me.jpg")}
+          hover="A készítő"
+          title="A készítő"
+          description="Keserű Kristóf vagyok, harmadéves DE IK PTI hallgató. Ezt az alkalmazás a szakdolgozati projectem. Az alkalmazást Dr. Kuki Attila mentorálásával készítettem el"
+        />
+      </Grid>
+      <Grid item xs>
+        <Typography component="div">
+          <Box textAlign="center" fontWeight={250} fontSize={40} m={1}>
+            Magamról
+          </Box>
+          <Box textAlign="left" m={1}>
+            Keserű Kristóf vagyok. Püspökladányban nőttem fel és itt is
+            végeztem el a középiskolát és a Debreceni Egyetem Informatika
+            karán folytattam tanulmányaimat Programtervező informatikus szakon.
+            Leginkább a webfejlesztés vonz és jelenleg is próbálok naprakész
+            tudást szerezni napjaink népszerűbb webtechnogiái terén.
+          </Box>
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
