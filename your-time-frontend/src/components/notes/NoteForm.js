442efefa9134a47a46ab519c70147c4ebd9055f9
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-03-22 13:09:24 +0100

    notes page újraírása

---
+++ your-time-frontend/src/components/notes/NoteForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import { MuiThemeProvider, Grid } from "@material-ui/core";
+import uuid from 'react-uuid'
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(2)
+    }
+  },
+  item: {
+    marginTop: 30,
+    marginBottom: 30
+  }
+}));
+
+const NoteForm = ({addNote}) => {
+  const classes = useStyles();
+
+  const [note, setNote] = useState({
+    id: "",
+    task: "",
+    completed: false
+  });
+
+  const handleTaskInputChange = e => {
+    setNote({ ...note, task: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (note.task.trim()) {
+      addNote({ ...note, id: uuid() })
+    }
+  }
+
+  return (
+    <MuiThemeProvider>
+      <form>
+        <Grid container justify="space-around" className={classes.root}>
+          <Grid item className={classes.item}>
+            <Input
+              value={note.task}
+              onChange={handleTaskInputChange}
+              name="task"
+              placeholder="Task"
+            />
+          </Grid>
+          <Grid item className={classes.item}>
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </MuiThemeProvider>
+  );
+};
+
+export default NoteForm;
--- your-time-frontend/src/pages/Notes.js
+++ your-time-frontend/src/pages/Notes.js
@@ -1,9 +1,25 @@
-import React from "react";
-import NotesComponent from "../components/notes/NotesComponent"
+import React, { useState } from "react";
+import NoteForm from "../components/notes/NoteForm";
+import { MuiThemeProvider, Grid } from "@material-ui/core";
 
 const Notes = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = note => {
+    setNotes([note, ...notes]);
+  };
+
   return (
-      <NotesComponent/>
+    <div>
+      <MuiThemeProvider>
+        <Grid container justify="space-around">
+          <Grid item>
+            <h1>Add note!</h1>
+          </Grid>
+        </Grid>
+      </MuiThemeProvider>
+      <NoteForm addNote={addNote}/>
+    </div>
   );
 };
 
