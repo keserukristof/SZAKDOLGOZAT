4d911c188dd577ee6a66088e1b71f83f5534d8ad
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-04-05 17:46:19 +0200

    react connection előkészítése

--- your-time-backend/app.js
+++ your-time-backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const timetableRoutes = require('./routes/timetable-routes')
+
 const app = express();
+
+app.use('/time-table', timetableRoutes)
 
 app.listen(5000);
